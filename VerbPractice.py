70d78dd2006272cc7c4c25147478f31526f4b7ab
chickendude <pokemaster103@gmail.com>
2015-05-02 22:21:30 +0800

    You can test a group of verbs by typing their type (nor, nor-nori, etc.)

--- VerbPractice.py
+++ VerbPractice.py
@@ -1,3 +1,8 @@
+'''
+maybe i can send a list with the verb name and tense you want to study for that verb.
+
+'''
+
 import re
 from collections import OrderedDict
 from random import randrange
@@ -14,13 +19,16 @@ IMPOSSIBLE = ( ('NI',('NERI','GURI')),
 		('ZUEK',('HIRI','ZURI','ZUEI')),
 		('HAIEK',()) )
 
+VERBTYPES = []
+EXIT = ('exit','quit','q','x')
+
 def clrScr(prompt=''):
 	# Clear the screen
 	print("\033c", end="")
 
 
-def numMenu(prompt,num):
-	''' Displays a menu, waits for a prompt, and returns the selected
+def numMenu(prompt,num,acceptableStrings=''):
+	'''	Displays a menu, waits for a prompt, and returns the selected
 		value. Repeats until a valid number is input.
 		
 		Inputs: prompt = the text to prompt the user
@@ -30,14 +38,17 @@ def numMenu(prompt,num):
 	prompt += "\n  {}. Exit".format(num+1)
 	while True:
 		clrScr()
-		print(prompt)
-		selection = input()
+		selection = input(prompt+"\n")
 		if selection:
+			if selection.lower() in acceptableStrings:
+				return selection.lower()
+			if selection.lower() in EXIT:
+				return False
 			try:
 				value = int(selection)
 			except:
 				# On exception, set value to an invalid number
-				value = num+1
+				value = -1
 			if value <= num and value > 0:
 				return value
 			if value == num+1:
@@ -60,21 +71,77 @@ def buildNorNori(nor,nori,tense):
 	string = stem + suffix + stem2
 	return string.lower()
 
+def multi_runTest(verbList,tense):
+	score = 0
+	grade = test = ""
+	while True:
+		# Pick a random verb in the list of verbs
+		verb = randrange(0,len(verbList))
+		clrScr()
+		print("*{}* - {} tense:\n{}".format(verbList[verb]['name'],tense,grade))
+		# Handle verbs of type NOR
+		if verbList[verb]['type'] == 'nor':
+			# Randomly select the galdegaia
+			nor = randrange(0,len(NOR))
+			test = "{}".format(NOR[nor])
+			selection = input(test+"\n")
+			answer = verbList[verb]['tenses'][tense]['nor'][nor]
+
+		# Handle verbs of type NOR-NORI
+		if verbList[verb]['type'] == 'nor-nori':
+			# Randomly select the galdegaiak
+			nor = randrange(0,len(NOR))
+			nori = randrange(0,len(NORI))
+			while NORI[nori] in IMPOSSIBLE[nor][1]:
+				nori = randrange(0,len(NORI))
+			test = "{} + {}".format(NOR[nor],NORI[nori])
+			selection = input(test+"\n")
+			nor = verbList[verb]['tenses'][tense]['nor'][nor]
+			nori = verbList[verb]['tenses'][tense]['nori'][nori]
+			answer = buildNorNori(nor,nori,tense)
+
+		# Handle verbs of type ZER-NORK
+		if verbList[verb]['type'] == 'zer-nork':
+			# Randomly select the galdegaia
+			zer = randrange(0,2)
+			nork = randrange(0,len(NORK))
+			person = ('singular','plural')
+			test = "{} ({})".format(NORK[nork],person[zer])
+			selection = input(test+"\n")
+			answer = verbList[verb]['tenses'][tense][person[zer]][nork]
+
+		# Handle verbs of type NORK
+		if verbList[verb]['type'] == 'nork':
+			# Randomly select the galdegaia
+			nork = randrange(0,len(NORK))
+			test = "{}".format(NORK[nork])
+			selection = input(test+"\n")
+			answer = verbList[verb]['tenses'][tense]['nork'][nork]
+
+		# If user types 'exit', exit routine
+		if selection in EXIT:
+			return
+		if selection.lower() == answer:
+			score += 1
+			grade = "{} correct!".format(score)
+		else:
+			grade = "* Incorrect: {} (you typed {}) - {} - {}".format(answer,selection,verbList[verb]['name'],test)
 
 def runTest(verb,tense):
 	''' verb is the verb's dictionary entry
 		tense is the tense id
 	'''
 	score = 0
-	grade = ""
+	grade = test = ""
 	while True:
 		clrScr()
-		print("Testing the {} tense of {}:\n{}".format(tense,verb['name'],grade))
+		print("{} - {} tense:\n{}".format(verb['name'],tense,grade))
 		# Handle verbs of type NOR
 		if verb['type'] == 'nor':
 			# Randomly select the galdegaia
 			nor = randrange(0,len(NOR))
-			selection = input("{}\n".format(NOR[nor]))
+			test = "{}".format(NOR[nor])
+			selection = input(test+"\n")
 			answer = verb['tenses'][tense]['nor'][nor]
 	
 		# Handle verbs of type NOR-NORI
@@ -84,7 +151,8 @@ def runTest(verb,tense):
 			nori = randrange(0,len(NORI))
 			while NORI[nori] in IMPOSSIBLE[nor][1]:
 				nori = randrange(0,len(NORI))
-			selection = input("{} + {}\n".format(NOR[nor],NORI[nori]))
+			test = "{} + {}".format(NOR[nor],NORI[nori])
+			selection = input(test+"\n")
 			nor = verb['tenses'][tense]['nor'][nor]
 			nori = verb['tenses'][tense]['nori'][nori]
 			answer = buildNorNori(nor,nori,tense)
@@ -95,24 +163,26 @@ def runTest(verb,tense):
 			zer = randrange(0,2)
 			nork = randrange(0,len(NORK))
 			person = ('singular','plural')
-			selection = input("{} ({})\n".format(NORK[nork],person[zer]))
+			test = "{} ({})".format(NORK[nork],person[zer])
+			selection = input(test+"\n")
 			answer = verb['tenses'][tense][person[zer]][nork]
 
 		# Handle verbs of type NORK
 		if verb['type'] == 'nork':
 			# Randomly select the galdegaia
 			nork = randrange(0,len(NORK))
-			selection = input("{}\n".format(NORK[nork]))
+			test = "{}".format(NORK[nork])
+			selection = input(test+"\n")
 			answer = verb['tenses'][tense]['nork'][nork]
 
 		# If user types 'exit', exit routine
-		if selection.lower() in ('exit','quit','q','x'):
+		if selection in EXIT:
 			return
 		if selection.lower() == answer:
 			score += 1
 			grade = "{} correct!".format(score)
 		else:
-			grade = "* Incorrect: {} (you typed {})".format(answer,selection)
+			grade = "* Incorrect: {} (you typed {}) - {} - {}".format(answer,selection,verb['name'],test)
 
 def pickTense(verb):
 	'''	Let's you choose the tense of the verb you want to study.
@@ -132,25 +202,28 @@ def pickTense(verb):
 	else:
 		return
 
-def pickVerb(verbList):
-	'''	Takes the list of verbs read from [verbs.lst] and prints a menu.
-		It sends whichever selection you made to pickTense().
-	'''
-	while True:
-		counter = 0
-		string = "Please pick a verb:"
-		verbtype = ""
-		for verb in verbList:
-			counter += 1
-			if verbtype != verb['type']:
-				verbtype = verb['type']
-				string += "\n"+verbtype.upper()+":"
-			string += "\n  "+str(counter)+". "+verb['name'].title()
-		selection = numMenu(string,len(verbs))
-		if selection:
-			pickTense(verbList[selection-1])
-		else:
-			return
+def multi_pickTense(verbs,verbtype):
+	# Build new list of verbs out of the verb type asked for
+	verbList = [verbs[i] for i in range(len(verbs)) if verbs[i]['type'] == verbtype]
+
+	# Find all the matching (intersecting) tenses
+	tenseList = set()
+	for verb in verbList:
+		# Set tenseList to the first verbs tense list
+		if not tenseList:
+			tenseList = set(verb['tenses'].keys())
+		tenseList = tenseList.intersection(verb['tenses'].keys())
+	tenseList = sorted(tenseList,reverse = True)
+	string = "{} verbs:\nWhat tense would you like to work on?".format(verbtype)
+	counter = 0
+	for tense in tenseList:
+		counter += 1
+		string += "\n  "+str(counter)+". "+str(tense.capitalize())
+	selection = numMenu(string,len(tenseList))
+	if selection:
+		multi_runTest(verbList,tenseList[selection-1])
+	else:
+		return
 
 
 if __name__ == '__main__':
@@ -163,6 +236,7 @@ if __name__ == '__main__':
 			if tabs == 0 and line.strip() != '':	# Skip blank/empty lines
 				if ':' in line:
 					verbtype = line[:-1]
+					VERBTYPES.append(verbtype)
 				else:
 					verbs.append({})
 					# Set verb name and verb type
@@ -175,4 +249,22 @@ if __name__ == '__main__':
 			if tabs == 2:
 				galdegaia, verbforms = line.strip().split(': ')
 				verbs[-1]['tenses'][verbtense][galdegaia] = verbforms.strip().split(', ')
-	pickVerb(verbs)
+	while True:
+		counter = 0
+		string = "Please pick a verb:"
+		verbtype = ""
+		for verb in verbs:
+			counter += 1
+			if verbtype != verb['type']:
+				verbtype = verb['type']
+				string += "\n"+verbtype.upper()+":"
+			string += "\n  "+str(counter)+". "+verb['name'].title()
+		selection = numMenu(string,len(verbs),VERBTYPES)
+		# Check if we are testing on a verb type or individual verb
+		if selection in VERBTYPES:
+			multi_pickTense(verbs,selection)
+		else:
+			if selection:
+				pickTense(verbs[selection-1])
+			else:
+				quit()
